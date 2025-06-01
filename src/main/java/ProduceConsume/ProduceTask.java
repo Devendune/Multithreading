38d8a20e6919e46130413e6be022ae7d4f037dd5
Devendune <devendunegi06@gmail.com>
2025-06-01 22:03:45 +0530

    Producer Consumer like

---
+++ src/main/java/ProduceConsume/ProduceTask.java
@@ -0,0 +1,23 @@
+package ProduceConsume;
+
+public class ProduceTask implements Runnable
+{
+    SharedResource sharedResource;
+
+    public  ProduceTask(SharedResource sharedResource)
+    {
+        this.sharedResource=sharedResource;
+    }
+
+    @Override
+    public void run()
+    {
+        System.out.println("Producer Thread "+Thread.currentThread().getName());
+        try {
+            Thread.sleep(5000l);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        sharedResource.addItem();
+    }
+}
